a4401176f99462f658b3db1cc00bf71bd801a8e4
Ale <idgaflolxx@gmail.com>
2026-04-01 20:40:04 -0300

    "Cors"

--- oauth3_project/settings.py
+++ oauth3_project/settings.py
@@ -38,13 +38,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'OAuth3',
 ]
 
 AUTH_USER_MODEL = "OAuth3.AppUser"
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
